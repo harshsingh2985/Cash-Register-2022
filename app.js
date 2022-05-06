46671ddc1d929cde91ded804c0c72b15aa35c528
Harsh Singh <88739283+uniqueharhsingh@users.noreply.github.com>
2022-05-06 04:17:55 -0700

    updating:error-messaage

--- app.js
+++ app.js
@@ -1,3 +1,40 @@
 const billamount=document.querySelector("#bill-amount")
+const cashGiven=document.querySelector("#cash-given")
+const checkButton=document.querySelector("#check-button")
+const message=document.querySelector("#error-message")
 
-console.log(billamount.value);
+checkButton.addEventListener("click",function validateBillAndCashAmount(){
+    // to block the message we have to write the event
+    message.style.display="none"
+    if(billamount.value>0){
+
+    }
+    else{
+        message.style.display="block"
+        message.innerText="The bill Amount Should Be Greater Than 0"
+    }
+})
+
+
+
+
+
+
+
+
+
+// checkButton.addEventListener("click",function clickhandler(){
+//     console.log("you have been clicked")
+
+// })
+
+
+
+
+
+
+// // console.log(checkbutton)
+// console.log(cashGiven.value)
+
+
+// console.log(billamount.value);
